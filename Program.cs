a465ab7bb6a497eaee70fb275a0d4b8b659fc329
chowwat67 <chowwat.si.67@ubu.ac.th>
2026-06-24 10:15:22 +0700

    Function Learn

--- Program.cs
+++ Program.cs
@@ -8,66 +8,58 @@ namespace LearnCS
 {
     internal class Program
     {
+        int x = 20;
+        int y = 7;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int a = 5;
-            int b = 2;
+            int a = 5, b = 2;
+            // Addition
             Console.WriteLine($"a + b = {a + b}");
-            //eiei
-            /*super
-              eiei
+            /* 
+               Multi-line comment
             */
-
+            // Arrays
             string[] cars = new string[3];
             cars[0] = "Ford";
             cars[1] = "BMW";
             cars[2] = "Toyota";
-            Console.WriteLine($"First car = {cars[0]}");
-            Console.WriteLine($"Second car = {cars[1]}");
-            Console.WriteLine($"Third car = {cars[2]}");
-            //Arrays
-
+            Console.WriteLine($"First car: {cars[0]}");
+            Console.WriteLine($"Second car: {cars[1]}");
+            Console.WriteLine($"Third car: {cars[2]}");
+            // List (Dynamic array)
             List<int> numbers = new List<int>();
-            numbers.Add(67);
-            numbers.Add(123);
-            numbers.Add(456);
-            Console.WriteLine($"First number = {numbers[0]}");
-            Console.WriteLine($"Second number = {numbers[1]}");
-            Console.WriteLine($"Third number = {numbers[2]}");
-            numbers.Remove(123);
-            Console.WriteLine($"After removing 123, Second number = {numbers[1]}");
-            numbers[0] = numbers[0] + numbers[1];
-            Console.WriteLine($"First number = {numbers[0]}");
-            //Lists (Dynamic Arrays)
-
-            Dictionary<string, float> product = new Dictionary<string, float>();
-            product["Apple"] = 0.99f;
-            product["Banana"] = 0.59f;
-            product["Orange"] = 0.79f;
-            Console.WriteLine($"Price of Apple = {product["Apple"]}");
-            Console.WriteLine($"Price of Banana = {product["Banana"]}");
-            Console.WriteLine($"Price of Orange = {product["Orange"]}");
-            //Dictionaries (Key-Value Pairs)
+            numbers.Add(64);
+            numbers.Add(128);
+            numbers.Add(256);
+            Console.WriteLine($"First number: {numbers[0]}");
+            Console.WriteLine($"Second number: {numbers[1]}");
+            Console.WriteLine($"Third number: {numbers[2]}");
+            numbers.Remove(128);
+            Console.WriteLine($"Second number: {numbers[1]}");
+            numbers.Add(100);
+            numbers[0] = numbers[1] + numbers[2];
+            Console.WriteLine($"First number: {numbers[0]}");
+            // Dictionaries
+            Dictionary<string, float> products = new Dictionary<string, float>();
+            products.Add("Apple", 0.99f);
+            products.Add("Banana", 0.59f);
+            products.Add("Orange", 0.79f);
+            Console.WriteLine($"Apple: ${products["Apple"]}");
+            Console.WriteLine($"Banana: ${products["Banana"]}");
+            Console.WriteLine($"Orange: ${products["Orange"]}");
 
             if (a > b) Console.WriteLine("Alright");
 
-            if (product.Count > 10)
-            {
-                Console.WriteLine("Products are available.");
-            }
-            else
-            {
-                Console.WriteLine("No products available.");
-            }
+            if (products.Count > 10) Console.WriteLine("Products available.");
+            else Console.WriteLine("Products unavailable.");
 
             int age = 15;
-            if (age >= 18) Console.WriteLine("You are an adults.");
+            if (age >= 18) Console.WriteLine("You are an adult.");
             else if (age >= 13) Console.WriteLine("You are a teenager.");
-            else Console.WriteLine("You are a child.");
-            //if-else Conditional Statements
+            else Console.WriteLine("You are not a child.");
 
-            char grade = 'A';
+            char grade = 'B';
             switch (grade)
             {
                 case 'A':
@@ -89,32 +81,54 @@ namespace LearnCS
                     Console.WriteLine("Invalid grade");
                     break;
             }
-            //switch Condition
 
             for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine($"For Loop Counts: {i}");
+                Console.WriteLine($"For loop iteration: {i}");
             }
-            //For Loop Condition
 
             bool isTrue = true;
             int counter = 0;
-            while (isTrue)
+            while (isTrue) // isTrue == true
             {
-                Console.WriteLine($"Eternity{counter + 1}");
+                Console.WriteLine("Eternity");
                 if (counter == 10)
                 {
                     isTrue = false;
                 }
                 counter++;
             }
-            //While Loop Condition
 
             foreach (string car in cars)
             {
-                Console.WriteLine($"Car: {cars}");
+                Console.WriteLine($"Car: {car}");
             }
-            //Foreach Condition
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine($"Car: {cars[i]}");
+            }
+
+            // Functions
+            string Hello(string name = "Noname")
+            {
+                return $"Hello, {name}!";
+            }
+
+            /* string h = Hello();
+            Console.WriteLine(h); */
+
+            Console.WriteLine(Hello("John"));
+            Console.WriteLine(Hello());
+
+            void NumberCompare(int x, int y)
+            {
+                if (x > y) Console.WriteLine($"{x} is greater than {y}");
+                else if (x < y) Console.WriteLine($"{x} is less than {y}");
+                else Console.WriteLine($"{x} is equal to {y}");
+            }
+
+            NumberCompare(5, 10);
         }
     }
 }
